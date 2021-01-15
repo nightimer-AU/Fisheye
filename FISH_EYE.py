4e05623251462ac8329cff8458ae2eff5212a909
Aditya Uppal <nightimer.au@gmail.com>
2021-01-15 22:26:50 +0530

    fixup

--- FISH_EYE.py
+++ FISH_EYE.py
@@ -6,7 +6,8 @@ try:
     from pygame import Color, Surface, SRCALPHA, RLEACCEL, BufferProxy
     from pygame.surfarray import pixels3d, array_alpha, pixels_alpha, array3d
     from pygame.image import frombuffer
-
+    import pathlib
+    import FishEyeAlgorithm
 except ImportError:
     print("\n<Pygame> library is missing on your system."
           "\nTry: \n   C:\\pip install pygame on a window command prompt.")
@@ -16,19 +17,19 @@ if __name__ == '__main__':
 
     w, h = 400, 400
     screen = pygame.display.set_mode((w * 2, h))
-
+    imagepath = pathlib.Path('images/EMARALD.jpg')
     background = pygame.image.load('back.png').convert()
     background.set_alpha(None)
 
-    surface24 = pygame.image.load("EMARALD.jpg").convert()
-    surface32 = pygame.image.load("EMARALD.jpg").convert_alpha()
+    surface24 = pygame.image.load(imagepath).convert()
+    surface32 = pygame.image.load(imagepath).convert_alpha()
 
     background = pygame.transform.smoothscale(background, (w * 2, h))
     surface24 = pygame.transform.smoothscale(surface24, (w, h))
     surface32 = pygame.transform.smoothscale(surface32, (w, h))
 
     i = 0
-    fisheye_surface = fish_eye32(surface32)
+    fisheye_surface = FishEyeAlgorithm.fish_eye(surface32)
     while 1:
         pygame.event.pump()
         keys = pygame.key.get_pressed()
